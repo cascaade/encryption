a3b0975c2117f54b2b4b58cbf7c7fc7665e72194
Owen <189063419+cascaade@users.noreply.github.com>
2026-02-07 10:35:50 -0500

    Update interface

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,25 +13,21 @@ public class Main {
             String option = scanner.nextLine();
             if (option.equals("e")) {
                 System.out.print("Enter a message: ");
+
                 String input = scanner.nextLine();
-
-                System.out.println("\n--- Encryption ---\n");
-
                 Message result = algorithm.encrypt(new Message(input, false));
-                System.out.print("Plaintext: ");
-                System.out.println(input);
-                System.out.print("Ciphertext: ");
+
+                System.out.print(input);
+                System.out.print(" >> [encryption] >> ");
                 System.out.println(result);
             } else if (option.equals("d")) {
                 System.out.print("Enter a message: ");
+
                 String input = scanner.nextLine();
-
-                System.out.println("\n--- Decryption ---\n");
-
                 Message result = algorithm.decrypt(new Message(input, true));
-                System.out.print("Ciphertext: ");
-                System.out.println(input);
-                System.out.print("Plaintext: ");
+
+                System.out.print(input);
+                System.out.print(" >> [decryption] >> ");
                 System.out.println(result);
             } else if (option.equals("q")) {
                 System.out.println("bye :(");
