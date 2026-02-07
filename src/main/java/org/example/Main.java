55d15765e9c11f260acfa674b1641230ee3ecb6d
Owen <189063419+cascaade@users.noreply.github.com>
2026-02-07 10:27:40 -0500

    Add CLI interface

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,44 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        EncryptionAlgorithm algorithm = new CaesarCipher(56);
+
+        while (true) {
+            System.out.print("\nEncrypt (e), decrypt (d), or quit (q)?: ");
+            String option = scanner.nextLine();
+            if (option.equals("e")) {
+                System.out.print("Enter a message: ");
+                String input = scanner.nextLine();
+
+                System.out.println("\n--- Encryption ---\n");
+
+                Message result = algorithm.encrypt(new Message(input, false));
+                System.out.print("Plaintext: ");
+                System.out.println(input);
+                System.out.print("Ciphertext: ");
+                System.out.println(result);
+            } else if (option.equals("d")) {
+                System.out.print("Enter a message: ");
+                String input = scanner.nextLine();
+
+                System.out.println("\n--- Decryption ---\n");
+
+                Message result = algorithm.decrypt(new Message(input, true));
+                System.out.print("Ciphertext: ");
+                System.out.println(input);
+                System.out.print("Plaintext: ");
+                System.out.println(result);
+            } else if (option.equals("q")) {
+                System.out.println("bye :(");
+                break;
+            } else {
+                System.out.println("Invalid statement.");
+            }
         }
     }
 }
