8218844a0d1b1d85f0c34cf90755b27c6d421fdd
Owen <189063419+cascaade@users.noreply.github.com>
2026-02-09 23:16:23 -0500

    ScrambleCipher base

---
+++ src/main/java/org/example/ScrambleCipher.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class ScrambleCipher implements EncryptionAlgorithm<Byte[]> {
+    @Override
+    public Message encrypt(Message plaintext, Byte[] key) {
+        if (key.length != 16)
+            throw new IllegalArgumentException("Unacceptable number of key bytes");
+
+        byte[] bytes = plaintext.toString().getBytes();
+
+        if (bytes.length != 16)
+            throw  new IllegalArgumentException("Unacceptable number of message bytes");
+
+        byte[][] cipherBytes = new byte[4][4];
+
+        for (int i = 0; i < bytes.length; i++) {
+            cipherBytes[i / 4][i % 4] = bytes[i];
+        }
+
+        byte[] outBytes = new byte[16];
+
+        int index = 0;
+        for (int i = 0; i < cipherBytes.length; i++) {
+            for (int j = 0; j < cipherBytes[i].length; j++) {
+                outBytes[index++] = cipherBytes[i][j];
+            }
+        }
+
+        return new Message(new String(outBytes, StandardCharsets.UTF_8), true);
+    }
+
+    @Override
+    public Message decrypt(Message ciphertext, Byte[] key) {
+        return new Message("not yet implemented", false);
+    }
+}
